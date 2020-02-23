3f189bf1fc22f1b3ca67130efaea9e22da888bc6
TomaszTomasiak <tomasz.tomasiak.pl@gmail.com>
2020-02-23 10:36:17 +0100

    zadanie 24.3

--- kodilla-patterns-part2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
+++ kodilla-patterns-part2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
@@ -9,14 +9,14 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 
 @Service
-public final class OrderFacade {
+public class OrderFacade {
     @Autowired
     private ShopService shopService;
 
     @Autowired
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
 
-    public void processOrder(final OrderDto order, final Long userId) throws OrderProcessingException {
+    public void processOrder(OrderDto order, Long userId) throws OrderProcessingException {
         boolean wasError = false;
         long orderId = shopService.openOrder(userId);
         if (orderId < 0) {
