0f2a666afa85b0f9d932a43efec91c45420b9778
TomaszTomasiak <55875232+TomaszTomasiak@users.noreply.github.com>
2020-04-08 15:45:32 +0200

    zadanie 31.2

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/testLoopAndConditionalBreakpoints.java
@@ -0,0 +1,20 @@
+package com.kodilla.testing.loop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class testLoopAndConditionalBreakpoints {
+    @Test
+    public void testLoop() {
+        //Given
+        long sum = 0;
+        //When
+        for (int i = 0; i < 1000; i++) {
+            sum += i;
+            System.out.println("[" + i +"] sum equals: " + sum );
+        }
+        //Then
+        assertEquals(499500, sum);
+    }
+}
