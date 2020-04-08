f8dbbfe8168ad8f1da1b29afc1b22fd2828568ac
TomaszTomasiak <55875232+TomaszTomasiak@users.noreply.github.com>
2020-04-08 17:21:26 +0200

    conflicts solving

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -79,7 +79,7 @@ public class TaskListDaoTestSuite {
         Task task1 = new Task("Test: Study Hibernate", 3);
         Task task2 = new Task("Test: Practice Named Queries", 6);
         Task task3 = new Task("Test: Study native queries", 7);
-        Task task4 = new Task("Test: Makse some tests", 13);
+        Task task4 = new Task("Test: Make some tests", 13);
 
         TaskFinancialDetails tfd1 = new TaskFinancialDetails(new BigDecimal(5), false);
         TaskFinancialDetails tfd2 = new TaskFinancialDetails(new BigDecimal(10), false);
@@ -119,8 +119,7 @@ public class TaskListDaoTestSuite {
             Assert.assertEquals(2, durationLongerThanTasks.size());
         } finally {
             //CleanUp
-            taskListDao.deleteById(id);
+            taskListDao.deleteAll();
         }
     }
-
 }
--- kodilla-spring-web/src/test/java/com/kodilla/spring/web/KodillaSpringWebApplicationTests.java
+++ kodilla-spring-web/src/test/java/com/kodilla/spring/web/KodillaSpringWebApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class KodillaSpringWebApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
